706b1bb2883b554005d423a30873acff81bdabe5
gittyDanny <daniilioffe2000@gmail.com>
2026-05-06 16:36:36 +0200

    Hello World test, Gradle Build

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Daniil",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Daniil")
+    }
+}
+
+@Composable
+fun MyApplicationTheme(content: @Composable () -> Unit) {
+    // Simple theme wrapper
+    androidx.compose.material3.MaterialTheme(
+        content = content
+    )
+}
